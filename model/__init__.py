bec8e5484373ab64d5aa1421178deff637dc0057
ashiofumiirerujiyu <ashiofumiirerujiyu@gmail.com>
2024-08-30 05:40:31 +0000

    Refactor: Add model

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,8 @@
-from model.generator import Generator
-from model.discriminator import Discriminator
+from model.GAN.generator import GANGenerator
+from model.GAN.discriminator import GANDiscriminator
+from model.CGAN.generator import CGANGenerator
+from model.CGAN.discriminator import CGANDiscriminator
+from model.DCGAN.generator import DCGANGenerator
+from model.DCGAN.discriminator import DCGANDiscriminator
 
-__all__ = ["Generator", "Discriminator"]
+__all__ = ['GANGenerator', 'GANDiscriminator', 'CGANGenerator', 'CGANDiscriminator', 'DCGANGenerator', 'DCGANDiscriminator']
